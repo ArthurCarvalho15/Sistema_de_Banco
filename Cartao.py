b49a4451624c28dfb4a239cc5834e37bb015f563
Arthur.Carvalho <arthur.carvalho@sempreceub.com>
2018-10-19 22:15:40 -0300

    Classe Cartão v1

---
+++ Cartao.py
@@ -0,0 +1,7 @@
+# Classe Cartao
+class Cartao:
+    # Método Contrutor
+    def __init__(self, numero, validade, security_code):
+        self.numero = numero
+        self.validade = validade
+        self.security_code = security_code
